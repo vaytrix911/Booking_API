dbfcd74b1fdf35b633623ced1d5c82ce76f25928
vaytrix911 <vaytrix911@gmail.com>
2026-08-28 23:34:32 +0100

    fixing error

--- app/models.py
+++ app/models.py
@@ -2,11 +2,13 @@ from sqlalchemy import Column,Integer,String,DateTime,ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 class User(Base):
+    __tablename__ = "users"
     id = Column(Integer,primary_key=True,index=True)
     username = Column(String,nullable=False,unique=True)
     password_hash = Column(String,nullable=False)
     appointments = relationship("Appointment")
 class Appointment(Base):
+    __tablename__ = "appointments"
     id = Column(Integer,primary_key=True,index=True)
     title = Column(String,nullable=True)
     description = Column(String,nullable=True)
