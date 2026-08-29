fbfe3f6af28b87be06d768c8f7180dcf003fa9fd
vaytrix911 <vaytrix911@gmail.com>
2026-08-29 01:19:02 +0100

    fix bug

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
-
-DATABASE_URL = "sqlite:///./booking.db"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
